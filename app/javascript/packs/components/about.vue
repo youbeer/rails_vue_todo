779e5523fd3e888480b80daa89d0b39243293b27
you <you@younoMacBook-Air.local>
2018-02-15 03:01:21 +0900

    first

---
+++ app/javascript/packs/components/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>about</p>
+    <p>
+      <a href="https://google.co.jp"Google></a>
+    </p>
+  </div>
+</template>
---
+++ app/javascript/packs/components/contact.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <!-- 内容はお好みで -->
+    <p>If you want to contact me, you send mail to below address.</p>
+  </div>
+</template>
